a7f510fbe26659d14c8bb4029e933ffa549b4f81
pydevazmi <pydevazmi@gmail.com>
2025-01-01 18:18:58 +0200

    Implement error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,29 @@
-import express, { RequestHandler } from 'express';
+import express, { ErrorRequestHandler, RequestHandler } from 'express';
 import moment from 'moment';
 import { createPostHandler, listPostsHandler } from './handlers/postHandlers';
 const app = express();
 
 app.use(express.json());
 
-const requestLoggerHandler:RequestHandler = (req, res, next) => {
-    console.log(`${moment().format("MM/DD/YYYY HH:mm:ss")} : [${res.statusCode}] ${req.method} ${req.path}`);
-    next();
+const requestLoggerHandler: RequestHandler = (req, res, next) => {
+  console.log(
+    `${moment().format('MM/DD/YYYY HH:mm:ss')} : [${res.statusCode}] ${req.method} ${req.path}`
+  );
+  next();
+};
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.error({ 'Uncaught Exeption Occured': err });
+  res.status(500).send('Internal Server Error');
+  next();
 };
 
 app.use(requestLoggerHandler);
 
-app.get("/posts", listPostsHandler);   
-app.post("/posts", createPostHandler);
+app.get('/posts', listPostsHandler);
+app.post('/posts', createPostHandler);
+app.use(errorHandler);
 
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
-})
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
