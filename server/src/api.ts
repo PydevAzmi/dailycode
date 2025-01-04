f70141f2f0e15d77ac7e4cfe168569587fc3aef4
pydevazmi <pydevazmi@gmail.com>
2025-01-04 13:32:45 +0200

    Refactor user authentication handlers; rename index.ts to server.ts and update related imports

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,15 +1,21 @@
-import { Post, User } from "./types";
+import { Post, User } from './types';
 
+// Post
 export type createPostRequest = Pick<Post, 'title' | 'url' | 'userId'>;
 export type createPostResponse = {};
 
+// user
 export type SignUpRequest = Pick<User, 'username' | 'firstName' | 'lastName' | 'email' | 'password'>;
-export interface SignUpResponse {};
-
+export interface SignUpResponse {}
 
 export interface SignInRequest {
-    login : string; // username or email
-    password : string;
+  login: string; // username or email
+  password: string;
 }
-
 export type SignInResponse = Pick<User, 'id' | 'username' | 'firstName' | 'lastName' | 'email'>;
+
+// comment
+
+// post
+
+// like
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,34 +3,33 @@ import moment from 'moment';
 import { createPostHandler, listPostsHandler } from './handlers/postHandlers';
 import asyncHandler from 'express-async-handler';
 import { InitDB } from './datastore';
-import { SignUpHandler } from './handlers/UserHandlers';
+import { SignUpHandler } from './handlers/AuthHandlers';
 
 (async () => {
   await InitDB();
   const app = express();
 
   app.use(express.json());
-  
+
   const requestLoggerHandler: RequestHandler = (req, res, next) => {
     console.log(`${moment().format('MM/DD/YYYY HH:mm:ss')} : [${res.statusCode}] ${req.method} ${req.path}`);
     next();
   };
-  
+
   const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     console.error({ 'Uncaught Exeption Occured': err });
     res.status(500).send('Internal Server Error');
     next();
   };
-  
+
   app.use(requestLoggerHandler);
-  
+
   app.get('/v1/posts', asyncHandler(listPostsHandler));
   app.post('/v1/posts', asyncHandler(createPostHandler));
   app.post('/v1/signup', asyncHandler(SignUpHandler));
   app.use(errorHandler);
-  
+
   app.listen(3000, () => {
     console.log('Server is running on port 3000');
   });
 })();
-
